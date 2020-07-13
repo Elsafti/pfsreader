4cd935c3e6ad005b30bb5f948f25653a163fc23f
Alexander Renz <alex.renz@gmx.net>
2020-07-13 12:24:56 +0200

    added test for .etv and .wbl

--- read_write_tests.py
+++ read_write_tests.py
@@ -12,16 +12,28 @@ print(os.getcwd())
 f = "./tests/MSHE_main.she"
 she_file = PfsFile(f)
 she_file.saveTo(f+".tmp")
-print("{}: \t{}".format(f, filecmp.cmp(f, f+".tmp", shallow=False)))
+print("{}: \t{}".format(filecmp.cmp(f, f+".tmp", shallow=False), f))
 
 # MSHE UZ property file
 f = "./tests/MSHE_UzSoilProp.uzs"
 she_file = PfsFile(f)
 she_file.saveTo(f+".tmp")
-print("{}: \t{}".format(f, filecmp.cmp(f, f+".tmp", shallow=False)))
+print("{}: \t{}".format(filecmp.cmp(f, f+".tmp", shallow=False), f))
 
 # MSHE Well file
 f = "./tests/MSHE_Wells.wel"
 she_file = PfsFile(f)
 she_file.saveTo(f+".tmp")
-print("{}: \t{}".format(f, filecmp.cmp(f, f+".tmp", shallow=False)))
+print("{}: \t{}".format(filecmp.cmp(f, f+".tmp", shallow=False), f))
+
+# MSHE water balance file
+f = "./tests/MSHE_waterbalance.wbl"
+she_file = PfsFile(f)
+she_file.saveTo(f+".tmp")
+print("{}: \t{}".format(filecmp.cmp(f, f+".tmp", shallow=False), f))
+
+# MSHE ETV file
+f = "./tests/MSHE_ETVegProp.ETV"
+she_file = PfsFile(f)
+she_file.saveTo(f+".tmp")
+print("{}: \t{}".format(filecmp.cmp(f, f+".tmp", shallow=False), f))
